0df6509b00389a76d219d8dba0e2714138592652
Paul Brousseau <object88@gmail.com>
2021-10-07 19:41:44 -0700

    chore: make buildable

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ export GO111MODULE=on
 
 if [ $(uname) == "Darwin" ]; then
   # Cannot do a static compilation on Darwin.
-  go build -ldflags "-s -w" -o bin/db main/main.go
+  go build -ldflags "-s -w" -o bin/annotator main/main.go
 else
   export CGO_ENABLED=0
-  go build -tags "netgo" -ldflags "-s -w -extldflags \"-static\"" -o bin/db main/main.go
+  go build -tags "netgo" -ldflags "-s -w -extldflags \"-static\"" -o bin/annotator main/main.go
 fi
